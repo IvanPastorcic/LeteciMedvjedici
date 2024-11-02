53a8c0f0e73eec4815d6e2b6f88b2a40cd0907e8
ivasulic <iva.sulic@fer.unizg.hr>
2024-11-02 12:27:42 +0100

    isprobavanje

---
+++ IzvorniKod/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import Student from './components/Student';
+import StudentList from './components/StudentList';
+import StudentForm from './components/StudentForm';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <StudentForm/>
+    </div>
+  );
+}
+
+export default App;
