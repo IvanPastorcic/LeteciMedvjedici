8c2766370c4cc222d60733a654667b975056cbfa
natalizegarac <natali.zegarac@fer.unizg.hr>
2024-11-06 22:52:02 +0100

    Added ReportRepository, ReportService, ReportController
    get all reports, add new report

--- IzvorniKod/backend/src/main/java/hr/fer/progi/backend/controller/ReportController.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/backend/controller/ReportController.java
@@ -1,8 +1,18 @@
 package hr.fer.progi.backend.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import hr.fer.progi.backend.model.Report;
+import hr.fer.progi.backend.repository.ReportRepository;
 import hr.fer.progi.backend.service.ReportService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class ReportController {
@@ -12,4 +22,18 @@ public class ReportController {
 	public ReportController(ReportService reportService) {
 		this.reportService = reportService;
 	}
+	
+	@GetMapping("/reports")
+	List<Report> reports() {
+		return reportService.getAllReports();
+	}
+	
+	@PostMapping("/reports")
+	Report newReport(@RequestBody Report newReport){
+		return reportService.newReport(newReport);
+	}
+	
+	
+
+	
 }
--- IzvorniKod/backend/src/main/java/hr/fer/progi/backend/repository/ReportRepository.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/backend/repository/ReportRepository.java
@@ -1,8 +1,9 @@
 package hr.fer.progi.backend.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import hr.fer.progi.backend.model.Report;
 
-public interface ReportRepository extends CrudRepository<Report, Integer> {
+public interface ReportRepository extends JpaRepository<Report, Long> {
 
 }
