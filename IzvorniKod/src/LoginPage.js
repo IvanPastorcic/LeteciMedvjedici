5d3836d3707b9ea220f258ba09ac5a1b80a14ea1
Lana Zekušić <lana.zekusic@fer.hr>
2024-11-07 21:55:00 +0100

    Added header component, edited registerForm.css

--- IzvorniKod/src/LoginPage.js
+++ IzvorniKod/src/LoginPage.js
@@ -4,30 +4,32 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 import Anonymous from "./components/Anonymous/Anonymous";
+import AnonHeader from "./components/AnonHeader/AnonHeader";
 import "./LoginPage.css";
 
 
 function LoginPage() {
-    return (
-      <Router>
-      <Routes>
-        <Route 
-          path="/" 
-          element={
-            <div>
+  return (
+    <Router>
+      <div>
+        <AnonHeader />
+        <Routes>
+          <Route 
+            path="/" 
+            element={
               <div className="FormContainer">
                 <Login />
                 <div className="Separator"></div>
                 <Register />
               </div>
-              <Anonymous />
-            </div>
-          } 
-        />
-        <Route path="/register" element={<RegisterForm />} />
-      </Routes>
+            } 
+          />
+          <Route path="/register" element={<RegisterForm />} />
+        </Routes>
+        <Anonymous />
+      </div>
     </Router>
-    );
+  );
 }
 
 export default LoginPage;
