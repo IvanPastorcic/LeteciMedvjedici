56e9ff0965f9c949c9af829b60c10125fa9f4fa7
Lana Zekušić <lana.zekusic@fer.hr>
2024-11-08 13:25:41 +0100

    Added info frame on login/register page

---
+++ IzvorniKod/src/components/FirstInfoFrame/FirstInfoFrame.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./FirstInfoFrame.css";
+
+function FirstInfoFrame() {
+  return (
+    <div className="FirstInfoFrame">
+      <div className="bigText">
+        <text>BE INFORMED</text>
+        <br></br>
+        <text>GET ALERTS</text>
+        <br></br>
+        <text>STAY SAFE</text>
+      </div>
+      <br></br>
+      <br></br>
+      <h id="with">with</h>
+      <br></br>
+
+      <h id="SafeBear">SafeBear</h>
+      <div className="Line"></div>
+      <p>our reliable, real time emergency reporting application</p>
+      <p>get information on available resources</p>
+    </div>
+  );
+}
+
+export default FirstInfoFrame;
