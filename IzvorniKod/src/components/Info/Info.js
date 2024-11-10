3211b2db057d1ffb71b415491e609886727f17dc
SilvijaRade <silvija.rade@fer.unizg.hr>
2024-11-10 19:03:52 +0100

    Dodavanje homepagea i razrada info komponente

---
+++ IzvorniKod/src/components/Info/Info.js
@@ -0,0 +1,17 @@
+const Info = () => {
+    return ( 
+        <div class= "Info">
+            <div className="info-title">
+                <text> FIRE</text>
+                <br></br>
+                <text> EARTHQUAKE</text>
+                <br></br>
+                <text> HEAVY STORM</text>
+                <br></br>
+                <text> LANDSLIDE</text>
+            </div>
+        </div>
+     );
+}
+ 
+export default Info;
