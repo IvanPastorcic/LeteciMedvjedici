5156b92b89424f234687f411e8b5dcfc4e2869e9
VuranE <elma.vuran@fer.unizg.hr>
2024-11-12 21:01:06 +0100

    Minor changes in login controller

--- IzvorniKod/backend/src/main/java/hr/fer/progi/backend/controller/LoginController.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/backend/controller/LoginController.java
@@ -1,15 +1,25 @@
 package hr.fer.progi.backend.controller;
 
+import hr.fer.progi.backend.model.Report;
+import hr.fer.progi.backend.repository.exception.InputIsNullException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//controller za provjeru autentifikacije. TODO: obrisati ovu klasu kasnije
+//controller za provjeru autentifikacije.
 @RestController
 public class LoginController {
 
-    @GetMapping("/")
-    public String hello(){
-        return "this is / endpoint";
+    @GetMapping("/login")
+    public ResponseEntity<String> login() {
+
+        return ResponseEntity.ok("Welcome user!");
+    }
+
+    @GetMapping("/home")
+    public ResponseEntity<String> home(){
+        return ResponseEntity.ok("Accessed home page");
     }
 }
