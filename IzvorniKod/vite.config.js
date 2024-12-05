96d6bae05e36bdbed49875c12c5506eb0224e04b
natalizegarac <natali.zegarac@fer.unizg.hr>
2024-12-05 20:50:50 +0100

    deleted vite.config

--- IzvorniKod/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    proxy: {
-      '/safebear': 'http://localhost:8081/safebear-backend',  // Make sure this matches your backend API's URL
-    },
-  },
-});
