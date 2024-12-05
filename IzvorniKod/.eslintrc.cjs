46e8d483baa5825df16f1afe7209639676905443
natalizegarac <natali.zegarac@fer.unizg.hr>
2024-12-05 19:00:26 +0100

    Update .eslintrc.cjs

--- IzvorniKod/.eslintrc.cjs
+++ IzvorniKod/.eslintrc.cjs
@@ -10,14 +10,8 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/prop-types': [
-      'warn'
-    ]
+    'react/prop-types': ['warn'],
   },
-}
+};
+
