f8c8012cbb2df846a1906f13599c2b9ddafa89c7
natalizegarac <natali.zegarac@fer.unizg.hr>
2024-12-11 17:47:37 +0100

    .

--- IzvorniKod/backend/src/main/java/hr/fer/progi/backend/db/DatabaseException.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/backend/db/DatabaseException.java
@@ -1,15 +1,15 @@
-package hr.fer.progi.backend.db;
-
-public class DatabaseException extends RuntimeException {
-
-
-
-        public DatabaseException(String message) {
-            super(message);
-        }
-        public DatabaseException(Exception e) {
-            super(e);
-        }
-
-
-}
+//package hr.fer.progi.backend.db;
+//
+//public class DatabaseException extends RuntimeException {
+//
+//
+//
+//        public DatabaseException(String message) {
+//            super(message);
+//        }
+//        public DatabaseException(Exception e) {
+//            super(e);
+//        }
+//
+//
+//}
