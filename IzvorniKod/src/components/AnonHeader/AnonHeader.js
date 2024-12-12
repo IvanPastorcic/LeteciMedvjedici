c563552dd46c85345add3f9472e55887dc76700e
ivasulic <iva.sulic@fer.unizg.hr>
2024-12-12 12:09:02 +0100

    added MapPage

--- IzvorniKod/src/components/AnonHeader/AnonHeader.js
+++ IzvorniKod/src/components/AnonHeader/AnonHeader.js
@@ -6,7 +6,7 @@ function AnonHeader() {
         <header className="anon-header">
             
             <img src="/logo.jpg" alt="Logo" className="logo" />
-            <text className="slogan">Need help? Want to report an emergency? Use SafeBear.</text>
+            <p className="slogan">Need help? Want to report an emergency? Use SafeBear.</p>
             
         </header>
     );
--- IzvorniKod/src/pages/MapPage/MapPage.js
+++ IzvorniKod/src/pages/MapPage/MapPage.js
@@ -1,12 +1,62 @@
-import AnonHeader from "../../components/AnonHeader/AnonHeader";
+// Import potrebnih biblioteka
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import './MapPage.css'; // Pretpostavlja se da je CSS podešen
+import AnonHeader from '../../components/AnonHeader/AnonHeader';
 
+// Rješavanje problema s marker ikonama
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const MapPage = () => {
-    return ( 
-        <div className="MapPage">
-            <AnonHeader />
-        </div>
-     );
-}
- 
+  const navigate = useNavigate();
+  return (
+    <div className="page-container">
+    <AnonHeader />
+    <button className="back-button" onClick={() => navigate('/home')}>
+        &lt; Back to newsfeed</button>
+    <div className="content-container">
+      <div className="map-wrapper">
+        <MapContainer
+          center={[45.1, 15.2]}
+          zoom={7}
+          scrollWheelZoom={true}
+          style={{ height: '100%', width: '100%' }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[45.1, 15.2]}>
+            <Popup>Dobrodošli na kartu Hrvatske!</Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+      <aside className="sidebar">
+        <h3>Filter by category</h3>
+        {['Fire', 'Earthquake', 'Heavy storms', 'Flooding', 'Landslide'].map((category, index) => (
+            <button key={index} className="filter-button">
+            {category}
+            </button>
+        ))}
+      </aside>
+
+    </div>
+  </div>
+  
+  
+  );
+};
+
 export default MapPage;
