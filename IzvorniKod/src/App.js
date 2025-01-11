8ba8c2f203a9b9960cf27e98f073408e4a0b9141
SilvijaRade <silvija.rade@fer.unizg.hr>
2025-01-11 12:59:23 +0100

    NeedsPage made

--- IzvorniKod/src/App.js
+++ IzvorniKod/src/App.js
@@ -15,6 +15,7 @@ import ManageResource from "./pages/ManageResource/ManageResource";
 
 import HomePageAdmin from "./pages/HomePageAdmin/HomePageAdmin";
 import ReportOpen from "./pages/ReportOpen/ReportOpen";
+import NeedsOpen from "./pages/NeedsOpen/NeedsOpen";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 
@@ -35,6 +36,7 @@ function App() {
 
         <Route path="/homeauthorities" element={<HomePageAuthorities />} />
         <Route path="/homehumanitarian" element={<HomePageHumanitarian />} />
+        <Route path="/needsopen" element={<NeedsOpen />} /> 
 
 
         <Route path="/addnewaction" element={<AddNewAction />} />
